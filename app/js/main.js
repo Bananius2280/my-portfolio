fee6c190e62eb802a58f0b0b75cdd3618d468db9
Bananius2280 <artemkharin3@gmail.com>
2022-11-27 17:26:13 +0200

    added more section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
 $(function () {
 
+
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
+  
+  $(".header-top__right-lists a").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 });
